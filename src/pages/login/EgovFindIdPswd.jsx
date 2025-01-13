cfedf77b0415a041e3ab36c3d3e363a2bb6dbbdf
minjoo <ignore4557@gmail.com>
2025-01-13 18:38:31 +0900

    아이디 비밀번호 찾기 페이지 css 수정

--- src/pages/login/EgovFindIdPswd.jsx
+++ src/pages/login/EgovFindIdPswd.jsx
@@ -25,13 +25,80 @@ function EgovFindIdPswd(props) {
                 </div>
                 {/* <!--// Location --> */}
 
-                <div className="layout">
-                    
+            <div className="layout">
 
+                <div className="contents" id="contents">
+                    {/* <!-- 본문 --> */}
+
+                    <div className="Flogin">
+                        <h1>ID/비밀번호 찾기</h1>
+
+
+                        <div className="box id_box">
+                            <form name="" method="" action="">
+                                <fieldset>
+                                    <legend>ID찾기</legend>
+                                    <h2>ID 찾기</h2>
+                                    <span className="group">
+                                        <input
+                                            type="text"
+                                            name=""
+                                            title="이름"
+                                            placeholder="이름"
+                                        />
+                                        <input
+                                            type="text"
+                                            name=""
+                                            title="이메일"
+                                            placeholder="이메일"
+                                        />
+                                        <button className="btn id_btn" type="button">
+                                        <span>아이디 찾기</span>
+                                        </button>
+                                      </span>
+                                </fieldset>
+                            </form>
+                        </div>
+
+                        <div className="box pswd_box">
+                            <form name="" method="" action="">
+                                <fieldset>
+                                    <legend>비밀번호 찾기</legend>
+                                    <h2>비밀번호 찾기</h2>
+                                    <span className="group">
+                                        <input
+                                            type="text"
+                                            name=""
+                                            title="아이디"
+                                            placeholder="아이디"
+                                        />
+                                        <input
+                                            type="text"
+                                            name=""
+                                            title="이름"
+                                            placeholder="이름"
+                                        />
+                                        <input
+                                            type="text"
+                                            name=""
+                                            title="이메일"
+                                            placeholder="이메일"
+                                        />
+                                        <button className="btn pswd_btn" type="button">
+                                        <span>비밀번호 찾기</span>
+                                        </button>
+                                      </span>
+                                </fieldset>
+                            </form>
+                        </div>
+
+                    </div>
+                    {/* <!-- 본문 --> */}
                 </div>
 
             </div>
             {/* <!-- c_wrap --> */}
+            </div>
         </div>
 
 );
