dd838d39c4b625aeae88c5f8451d65f6826d28d0
김은중 <gearpoint12@dev-jitsu.com>
2025-01-23 17:03:35 +0900

    git 관련

--- src/EgovMain.test.jsx
+++ src/EgovMain.test.jsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { describe, it } from "vitest";
-import EgovMain from "@/pages/main/EgovMain";
+import EgovMain from "@/pages/main/EgovMainUser";
 
 describe("EgovMain Component", () => {
   it("renders correctly", () => {
