75ac791324608268c1916e423b7324d39343d3a4
김은중 <gearpoint12@dev-jitsu.com>
2025-02-05 10:53:22 +0900

    에디터 최소 높이 200px으로 지정

--- src/components/CommonEditor.jsx
+++ src/components/CommonEditor.jsx
@@ -1,21 +1,38 @@
+import React, { memo, useState } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import translations from 'ckeditor5/translations/ko.js';
+import "@/css/commonEditor.css";
 
-const CommonEditor = ({ value, onChange}) => {
+const CommonEditor = memo(({ value, onChange}) => {
+
+    const [editorLoaded, setEditorLoaded] = useState(false);
+
+    useState(() => {
+        import('@ckeditor/ckeditor5-react').then((module) => {
+            setEditorLoaded(true);
+        });
+    }, []);
+
     return (
-        <CKEditor
-            editor={ClassicEditor}
-            data={value}
-            onChange={(event, editor) => onChange(editor.getData())}
+        <>
+        {editorLoaded ? (
+            <CKEditor
+                editor={ClassicEditor}
+                data={value}
+                onChange={(event, editor) => onChange(editor.getData())}
 
-            config={{
-                licenseKey: "GPL",
-                language: 'ko',
-                translations: [translations]
-            }}
-        />
-    )
-}
+                config={{
+                    licenseKey: "GPL",
+                    language: 'ko',
+                    translations: [translations]
+                }}
+            />
+        ) : (<p>에디터를 로딩중입니다.</p>)
+        }
+        </>
+    );
+
+});
 
 export default CommonEditor;
