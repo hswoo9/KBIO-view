bc5a5eda057ff82f67041a227af60378a4bad53b
김은중 <gearpoint12@dev-jitsu.com>
2025-02-05 11:54:06 +0900

    ck에디터 이미지 업로드 추가

--- src/components/CommonEditor.jsx
+++ src/components/CommonEditor.jsx
@@ -3,10 +3,47 @@ import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import translations from 'ckeditor5/translations/ko.js';
 import "@/css/commonEditor.css";
+import * as EgovNet from "@/api/egovFetch";
+import CODE from "@/constants/code";
 
 const CommonEditor = memo(({ value, onChange}) => {
 
     const [editorLoaded, setEditorLoaded] = useState(false);
+
+    const customUploadAdapter = (loader) => {
+        return {
+            upload(){
+                return new Promise ((resolve, reject) => {
+                   const formData = new FormData();
+                   loader.file.then( (file) => {
+                       formData.append("files", file);
+                       const requestOptions = {
+                           method: "POST",
+                           body: formData
+                       };
+                       EgovNet.requestFetch("/commonApi/setCkEditorFiles", requestOptions, (resp) => {
+                           if (Number(resp.resultCode) === Number(CODE.RCV_SUCCESS)) {
+                               console.log(resp);
+                                resolve({
+                                    default: window.location.origin + resp.result.files[0].atchFilePathNm +
+                                        resp.result.files[0].strgFileNm + "." +
+                                        resp.result.files[0].atchFileExtnNm
+                                })
+                           } else {
+                           }
+                       });
+
+                   })
+                });
+            }
+        }
+    }
+
+    function customUploadPlugin ( editor) {
+        editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+            return customUploadAdapter(loader);
+        }
+    }
 
     useState(() => {
         import('@ckeditor/ckeditor5-react').then((module) => {
@@ -25,7 +62,9 @@ const CommonEditor = memo(({ value, onChange}) => {
                 config={{
                     licenseKey: "GPL",
                     language: 'ko',
-                    translations: [translations]
+                    translations: [translations],
+                    extraPlugins: [customUploadPlugin]
+
                 }}
             />
         ) : (<p>에디터를 로딩중입니다.</p>)
