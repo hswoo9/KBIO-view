679537d6d4cc3adf410768219d5aa942a04d15bf
hswoo9 <gksthe@naver.com>
2025-02-10 14:38:59 +0900

    마이페이지 회원탈퇴 모바일패스 제외 완료

--- src/pages/mypage/MemberMyPageIdentity.jsx
+++ src/pages/mypage/MemberMyPageIdentity.jsx
@@ -1,143 +1,134 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import { Link, NavLink, useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
+import * as EgovNet from "@/api/egovFetch";
 import URL from "@/constants/url";
+import CODE from "@/constants/code";
+import 'moment/locale/ko';
+import ManagerLeft from "@/components/manager/ManagerLeftMember";
+import EgovRadioButtonGroup from "@/components/EgovRadioButtonGroup";
+import Swal from "sweetalert2";
+import base64 from 'base64-js';
+import { getSessionItem } from "@/utils/storage";
 import passImage from '@/assets/images/ico_pass.png';
 import mobileImage from '@/assets/images/ico_mobile.png';
+import { removeSessionItem } from "@/utils/storage";
 
 const MemberMyPageIdentity = () => {
     const [isVerified, setIsVerified] = useState(false);
     const navigate = useNavigate();
     const location = useLocation();
+    const sessionUser = getSessionItem("loginUser");
+    const sessionUserName = sessionUser?.name;
+    const sessionUserSn = sessionUser?.userSn;
 
-    const [signupType, setSignupType] = useState("");
 
-    useEffect(() => {
-        if (location.state?.signupType) {
-            setSignupType(location.state.signupType);
+    const handleImageClick = () => {
+        if (!isVerified) {
+            Swal.fire("안내 사항에 동의해야 합니다.");
+            return;
         }
-    }, [location.state]);
+        const CancelMemberUrl = `/memberApi/myPageCancelMember`;
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+                userSn: sessionUserSn 
+            }),
+        };
+
+        EgovNet.requestFetch(CancelMemberUrl, requestOptions, (resp) => {
+            if (Number(resp.resultCode) === Number(CODE.RCV_SUCCESS)) {
+                removeSessionItem("loginUser");
+                Swal.fire("회원탈퇴가 완료되었습니다.");
+                navigate(URL.LOGIN);
+            } else {
+                Swal.fire("ERR : " + resp.resultMessage);
+            }
+        });
+    };
 
 
     const handlePrev = (type) => {
-        navigate(URL.TERMS_AGREEMENT, { state: { signupType: type } });
+        navigate(URL.MEMBER_MYPAGE_CANCEL);
     }
-    const handleNext = () => {
-        if (location.state?.signupType) {
-            navigate(URL.MYPAGE_CREATE, { state: { signupType } });
-        }else{
-            navigate(URL.MYPAGE_CREATE, { state : location.state} );
-        }
-    };
 
     return (
         <div id="container" className="container withdraw join_step">
             <div className="inner">
                 <ul className="stepWrap" data-aos="fade-up" data-aos-duration="1500">
                     <li>
-                        <div className="num"><p>1</p></div>
-                        <p className="text">약관동의</p>
+                        <NavLink to={URL.MEMBER_MYPAGE_MODIFY} activeClassName="active">
+                            <div className="num"><p>1</p></div>
+                            <p className="text">회원정보수정</p>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={URL.MEMBER_MYPAGE_CONSULTING} activeClassName="active">
+                            <div className="num"><p>2</p></div>
+                            <p className="text">컨설팅의뢰 내역</p>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={URL.MEMBER_MYPAGE_SIMPLE} activeClassName="active">
+                            <div className="num"><p>3</p></div>
+                            <p className="text">간편상담 내역</p>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={URL.MEMBER_MYPAGE_DIFFICULTIES} activeClassName="active">
+                            <div className="num"><p>4</p></div>
+                            <p className="text">애로사항 내역</p>
+                        </NavLink>
                     </li>
                     <li className="active">
-                        <div className="num"><p>2</p></div>
-                        <p className="text">본인인증</p>
-                    </li>
-                    <li>
-                        <div className="num"><p>3</p></div>
-                        <p className="text">정보입력</p>
-                    </li>
-                    <li>
-                        <div className="num"><p>4</p></div>
-                        <p className="text">신청완료</p>
+                        <NavLink to={URL.MEMBER_MYPAGE_CANCEL} activeClassName="active">
+                            <div className="num"><p>5</p></div>
+                            <p className="text">회원탈퇴</p>
+                        </NavLink>
                     </li>
                 </ul>
 
-                <div className="titleWrap type1" data-aos="fade-up" data-aos-duration="1500">
-                    <p className="tt1">본인 인증</p>
-                    <strong className="tt2">사용할 인증을 선택해주세요</strong>
-                </div>
-
-                <div className="authenBox" data-aos="fade-up" data-aos-duration="1500">
-                    <div className="selectBox">
-                        <button type="button" className="pass" onClick={handleNext}>
-                            <figure className="imgBox">
-                                <img src={passImage} alt="pass images"/>
-                            </figure>
-                            <p className="tt">통신사 PASS</p>
-                        </button>
-                        <button type="button" className="moblie" onClick={handleNext}>
-                            <figure className="imgBox">
-                                <img src={mobileImage} alt="mobile ID"/>
-                            </figure>
-                            <p className="tt">모바일 신분증</p>
-                        </button>
+                <div className="inner">
+                    <div className="titleWrap type1" data-aos="fade-up" data-aos-duration="1500">
+                        <p className="tt1">안내사항</p>
+                        <strong className="tt2">회원탈퇴를 신청하기전, 다음 내용을 꼭 확인해주세요.</strong>
                     </div>
-                    <div className="textBox">
-                        <p>간편인증을 이용하기 위해서는 [휴대폰 본인확인]이 필요합니다.</p>
-                        <p>모바일 신분증앱에서 모바일 신분증을 등록 후 사용할 수 있습니다.</p>
+                    <div className="topTextBox" data-aos="fade-up" data-aos-duration="1500">
+                        <p>회원정보 및 개인형 서비스 이용 기록은 개인정보보호 처리 방침 기준에 따라 삭제됩니다.</p>
+                        <p>회원탈퇴시 더 이상 홈페이지 서비스 사용이 불가능하며, K-BIO LabHub 홈페이지에서 탈퇴처리됩니다.</p>
+                        <label className="checkBox type1"><small>안내 사항을 모두 확인하였으며 , 이에 동의합니다.</small><input
+                            type="checkbox"
+                            id="agree"
+                            name="agree"
+                            checked={isVerified}
+                            onChange={() => setIsVerified(!isVerified)}
+                        /></label>
                     </div>
-                    <button type="button" className="clickBtn black" onClick={() => handlePrev("general")}>
-                        <span>뒤로가기</span></button>
+                    <div className="authenBox" data-aos="fade-up" data-aos-duration="1500">
+                        <div className="selectBox">
+                            <button type="button" className="pass" onClick={handleImageClick}>
+                            <figure className="imgBox"><img src={passImage} alt="pass images"/>
+                                </figure>
+                                <p className="tt">통신사 PASS</p>
+                            </button>
+                            <button type="button" className="moblie" onClick={handleImageClick}>
+                                <figure className="imgBox"><img src={mobileImage} alt="pass images"/>
+                                </figure>
+                                <p className="tt">모바일 신분증</p>
+                            </button>
+                        </div>
+                        <div className="textBox">
+                            <p>간편인증을 이용하기 위해서는 [휴대폰 본인확인]이 필요합니다.</p>
+                            <p>모바일 신분증앱에서 모바일 신분증을 등록 후 사용할 수 있습니다.</p>
+                        </div>
+                        <button type="button" className="clickBtn black"><span>뒤로가기</span></button>
+                    </div>
                 </div>
             </div>
         </div>
-        /*<div className="container">
-            <div className="c_wrap">
-                {/!* Location *!/}
-                <div className="location">
-                    <ul>
-                        <li>
-                            <a className="home" href="#!">
-                                Home
-                            </a>
-                        </li>
-                        <li>본인인증</li>
-                    </ul>
-                </div>
-
-                {/!* Layout *!/}
-                <div className="layout">
-                    <div className="contents VERIFICATION" id="contents">
-                        {/!* Header *!/}
-                        <div className="top_tit">
-                            <h1 className="tit_1">본인인증</h1>
-                            <p className="desc">
-                                회원가입을 위해 본인인증을 진행해주세요.
-                            </p>
-                        </div>
-
-                        {/!* Verification *!/}
-                        <div className="verification_wrap">
-                            <button
-                                className="btn btn_skyblue_h46 w_100"
-                                onClick={handleVerification}
-                            >
-                                본인인증
-                            </button>
-                            {isVerified && (
-                                <p className="success_message">
-                                    본인인증이 완료되었습니다.
-                                </p>
-                            )}
-                        </div>
-
-                        {/!* Button Area *!/}
-                        <div className="board_btn_area">
-                            <div className="left_col btn1">
-                                <button
-                                    className={`btn btn_skyblue_h46 w_100 ${
-                                        isVerified ? "" : "btn_disabled"
-                                    }`}
-                                    onClick={handleNext}
-                                    disabled={!isVerified}
-                                >
-                                    다음
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>*/
     )
         ;
 };
