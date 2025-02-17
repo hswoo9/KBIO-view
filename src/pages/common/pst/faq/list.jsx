272519593b73774034c215ad0a7121a6993fa2f1
김은중 <gearpoint12@dev-jitsu.com>
2025-02-17 14:27:02 +0900

    faq 퍼블 작업

--- src/pages/common/pst/faq/list.jsx
+++ src/pages/common/pst/faq/list.jsx
@@ -6,10 +6,11 @@ import URL from "@/constants/url";
 import CODE from "@/constants/code";
 import 'moment/locale/ko';
 import ManagerLeftNew from "@/components/manager/ManagerLeftNew";
-
+import CommonSubMenu from "@/components/CommonSubMenu";
 import Swal from 'sweetalert2';
+import AOS from "aos";
 import EgovPaging from "@/components/EgovPaging";
-
+import EgovUserPaging from "@/components/EgovUserPaging";
 import moment from "moment/moment.js";
 import {getSessionItem} from "../../../../utils/storage.js";
 
@@ -37,7 +38,6 @@ function commonPstList(props) {
     const [bbs, setBbs] = useState({});
     const [pstList, setPstList] = useState([]);
     const [paginationInfo, setPaginationInfo] = useState({});
-
     const searchTypeRef = useRef();
     const searchValRef = useRef();
 
@@ -47,6 +47,14 @@ function commonPstList(props) {
             getPstList(searchDto);
         }
     };
+
+    const toggleLi = (e) => {
+        if(e.currentTarget.closest("li").classList.contains("open")){
+            e.currentTarget.closest("li").classList.remove("open");
+        }else{
+            e.currentTarget.closest("li").classList.add("open");
+        }
+    }
 
     const getPstList = useCallback(
         (searchDto) => {
@@ -66,29 +74,34 @@ function commonPstList(props) {
                     setBbs(resp.result.bbs);
                     let dataList = [];
                     dataList.push(
-                        <tr>
-                            <td colSpan={resp.result.bbs.atchFileYn == "Y" ? "3" : "2"}>검색된 결과가 없습니다.</td>
-                        </tr>
+                        <li key="no_data">
+                            <a>
+                                <div className="questionBox">
+                                    <i className="point">Q</i>
+                                    <p className="text">
+                                        검색된 결과가 없습니다.
+                                    </p>
+                                </div>
+                            </a>
+                        </li>
                     );
 
                     resp.result.pstList.forEach(function (item, index) {
                         if (index === 0) dataList = []; // 목록 초기화
                         dataList.push(
-                            <tr key={item.pstSn}>
-                                <td>
-                                    {item.upendNtcYn == "Y" ?
-                                        "공지" :
-                                        resp.paginationInfo.totalRecordCount - (resp.paginationInfo.currentPageNo - 1) * resp.paginationInfo.pageSize - index}
-                                </td>
-                                <td style={{textAlign: "left", paddingLeft: "15px"}}>
-                                    <a onClick={() => {sessionUser.userSe == "ADM" ? moveToPstDetail(item.pstSn) : ""}} style={{cursor:"pointer"}}>
-                                        {item.pstTtl}
-                                    </a>
-                                </td>
-                                {resp.result.bbs.atchFileYn == "Y" && (
-                                    <td>{item.fileCnt != 0 ? "있음" : "없음"}</td>
-                                )}
-                            </tr>
+                            <li key={item.pstSn}
+                                onClick={toggleLi}
+                                className="toggleLi"
+                            >
+                                <a style={{cursor: "pointer"}}>
+                                    <div className="questionBox">
+                                        <i className="point">Q</i>
+                                        <p className="text">{item.pstTtl}</p>
+                                    </div>
+                                    <div className="answerBox" dangerouslySetInnerHTML={{__html: item.pstCn}}>
+                                    </div>
+                                </a>
+                            </li>
                         );
                     });
                     setPstList(dataList);
@@ -104,99 +117,91 @@ function commonPstList(props) {
 
     useEffect(() => {
         getPstList(searchDto);
+        AOS.init();
     }, []);
 
     const moveToPstDetail = (pstSn) => {
         navigate(
             { pathname: URL.COMMON_PST_FAQ_DETAIL },
-            { state: { pstSn:  pstSn} },
+            { state: {
+                    pstSn:  pstSn,
+                    menuSn : location.state?.menuSn,
+                    menuNmPath : location.state?.menuNmPath,
+                } },
             { mode:  CODE.MODE_READ}
         );
     }
 
     return (
-        <div id="container" className="container layout cms">
+        <div id="container" className="container faq board">
             <div className="inner">
-                <h2 className="pageTitle">{bbs.bbsNm}</h2>
-
-                <div className="cateWrap inputBox type1" style={{flexDirection: "row"}}>
-                    <div className="itemBox" style={{width: "7%"}}>
-                        <select
-                            id="bbsTypeNm"
-                            name="bbsTypeNm"
-                            title="검색유형"
-                            ref={searchTypeRef}
-                            style={{width: "50%"}}
-                            onChange={(e) => {
-                                setSearchDto({...searchDto, searchType: e.target.value})
-                            }}
-                        >
-                            <option value="">전체</option>
-                            <option value="pstTtl">제목</option>
-                            <option value="pstCn">내용</option>
-                        </select>
+                <CommonSubMenu/>
+                <div className="inner2">
+                    <div className="searchFormWrap type2" data-aos="fade-up" data-aos-duration="1500">
+                        <form>
+                            <div className="searchBox">
+                                <div className="itemBox type2">
+                                    <select
+                                        id="bbsTypeNm"
+                                        name="bbsTypeNm"
+                                        title="검색유형"
+                                        className="niceSelectCustom"
+                                        ref={searchTypeRef}
+                                        style={{backgroundColor: "#fff"}}
+                                        onChange={(e) => {
+                                            setSearchDto({...searchDto, searchType: e.target.value})
+                                        }}
+                                    >
+                                        <option value="">전체</option>
+                                        <option value="pstTtl">제목</option>
+                                        <option value="pstCn">내용</option>
+                                    </select>
+                                </div>
+                                <div className="inputBox type1">
+                                    <label className="input">
+                                        <input
+                                            name=""
+                                            defaultValue={
+                                                searchDto && searchDto.searchVal
+                                            }
+                                            placeholder=""
+                                            ref={searchValRef}
+                                            onChange={(e) => {
+                                                setSearchDto({...searchDto, searchVal: e.target.value})
+                                            }}
+                                            onKeyDown={activeEnter}
+                                            placeholder="검색어를 입력해주세요."
+                                        />
+                                    </label>
+                                </div>
+                                <button type="button" className="searchBtn"
+                                        onClick={() => {
+                                            getPstList({
+                                                ...searchDto,
+                                                pageIndex: 1,
+                                                searchType: searchTypeRef.current.value,
+                                                searchVal: searchValRef.current.value,
+                                            });
+                                        }}
+                                >
+                                    <div className="icon"></div>
+                                </button>
+                            </div>
+                            <div className="tabBox type1">
+                                <div className="bg hover"></div>
+                                <ul className="list">
+                                    <li className="active"><a href="#"><span>사이트</span></a></li>
+                                    <li><a href="#"><span>기관</span></a></li>
+                                    <li><a href="#"><span>컨설팅</span></a></li>
+                                </ul>
+                            </div>
+                        </form>
                     </div>
-                    <div className="itemBox">
-                        <label className="input">
-                            <input type="text"
-                                   name=""
-                                   defaultValue={
-                                       searchDto && searchDto.searchVal
-                                   }
-                                   placeholder=""
-                                   ref={searchValRef}
-                                   onChange={(e) => {
-                                       setSearchDto({...searchDto, searchVal: e.target.value})
-                                   }}
-                                   onKeyDown={activeEnter}
-                            />
-                        </label>
-                    </div>
-                    <div className="rightBtn">
-                        <button type="button"
-                                className="searchBtn btn btn1 point"
-                                onClick={() => {
-                                    getPstList({
-                                        ...searchDto,
-                                        pageIndex: 1,
-                                        searchType: searchTypeRef.current.value,
-                                        searchVal: searchValRef.current.value,
-                                    });
-                                }}
-                                style={{color: "#fff", width: "100px"}}
-                        >
-                            조회
-                        </button>
-                    </div>
-                </div>
-                <div className="contBox board type1 customContBox">
-                    <div className="topBox"></div>
-                    <div className="tableBox type1">
-                        <table>
-                            <caption>게시글목록</caption>
-                            <colgroup>
-                                <col width="80"/>
-                                <col/>
-                                {bbs.atchFileYn == "Y" && (
-                                    <col width="80"/>
-                                )}
-                            </colgroup>
-                            <thead>
-                            <tr>
-                                <th>번호</th>
-                                <th>제목</th>
-                                {bbs.atchFileYn == "Y" && (
-                                    <th>첨부파일</th>
-                                )}
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {pstList}
-                            </tbody>
-                        </table>
-                    </div>
+                    <ul className="listBox" data-aos="fade-up" data-aos-duration="1500">
+                        {pstList}
+                    </ul>
                     <div className="pageWrap">
-                        <EgovPaging
+                        <EgovUserPaging
                             pagination={paginationInfo}
                             moveToPage={(passedPage) => {
                                 getPstList({
@@ -205,15 +210,6 @@ function commonPstList(props) {
                                 });
                             }}
                         />
-                        {authrt.wrtAuthrt == "Y" && sessionUser && (
-                            <NavLink
-                                to={URL.COMMON_PST_FAQ_CREATE}
-                                state={{bbsSn: bbs.bbsSn}}
-                                mode={CODE.MODE_CREATE}
-                            >
-                                <button type="button" className="writeBtn clickBtn"><span>등록</span></button>
-                            </NavLink>
-                        )}
                     </div>
                 </div>
             </div>
