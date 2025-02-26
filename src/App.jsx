2cfd96ba0633b255901487cb65bb4c7c3f3b5326
김은중 <gearpoint12@dev-jitsu.com>
2025-02-26 16:34:49 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import RootRoutes from "@/routes";
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
+
+    useEffect(() => {
+        console.log(
+            "%c DEV-JITSU ",
+            "font-size: 28px; font-weight: bold; color: white; background: linear-gradient(45deg, #ff416c, #ff4b2b); padding: 12px 32px; border-radius: 10px; text-shadow: 3px 3px 6px rgba(0, 0, 0, 0.3); box-shadow: 0 4px 10px rgba(255, 75, 43, 0.6); font-family: 'Arial', sans-serif;"
+        );
+    }, []);
+
     return (
         <Router>
             <RootRoutes />
