588b21cb2525af76ec41124ca6507f9c665c8373
hswoo9 <gksthe@naver.com>
2025-03-05 16:12:14 +0900

    산하직원수정 모달창 css수정중

--- src/components/CompanyModifyMember.jsx
+++ src/components/CompanyModifyMember.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect, useCallback} from "react";
-import { useDropzone } from 'react-dropzone';
+import React, { useState, useEffect, useRef } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import Swal from "sweetalert2";
 import URL from "@/constants/url";
 import CODE from "@/constants/code";
@@ -8,11 +8,16 @@ import * as ComScript from "@/components/CommonScript";
 import { getComCdList } from "@/components/CommonComponents";
 import CommonEditor from "@/components/CommonEditor";
 import moment from "moment/moment.js";
+import { getSessionItem } from "@/utils/storage";
 
-const CompanyModifyMember = ({data, onSave}) => {
+const CompanyModifyMember = ({data}) => {
+    const sessionUser = getSessionItem("loginUser");
+    const navigate = useNavigate();
     const [paramsData, setParamsData] = useState({});
+    const sessionUserSn = sessionUser?.userSn;
 
     useEffect(() => {
+        console.log(data);
         setParamsData(data);
     }, [data]);
 
@@ -37,18 +42,64 @@ const CompanyModifyMember = ({data, onSave}) => {
         }).open();
     };
 
+    const pwdReset = () => {
+        Swal.fire({
+            title: `<span style="font-size: 14px; line-height: 0.8;">
+                    회원의 비밀번호를 초기화를 할 경우<br>
+                    <strong>qwer12!@</strong><br>
+                    로 초기화 됩니다.<br>
+                    해당 회원의 비밀번호를 초기화 하시겠습니까?
+                </span>
+            `,
+            showCloseButton: true,
+            showCancelButton: true,
+            confirmButtonText: "예",
+            cancelButtonText: "아니오"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const resetPwdUrl = "/memberApi/resetMemberPassword";
+                const requestOptions = {
+                    method: "POST",
+                    headers: {
+                        "Content-type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        ...companyMember,
+                        userPw: "qwer12!@"
+                    }),
+                };
+
+                EgovNet.requestFetch(resetPwdUrl, requestOptions, (resp) => {
+                    if (Number(resp.resultCode) === Number(CODE.RCV_SUCCESS)) {
+                        Swal.fire({
+                            title: "비밀번호가 초기화되었습니다.",
+                            confirmButtonText: "확인"
+                        })
+                    } else {
+                        Swal.fire({
+                            title: "오류가 발생했습니다.",
+                            text: resp.resultMessage,
+                            confirmButtonText: "확인"
+                        });
+                    }
+                });
+            }
+        });
+    };
+
 
 
     useEffect(() => {
-        setCompanyMember(paramsData);
+        setCompanyDetail(paramsData);
     }, [paramsData]);
 
-    const [companyMember, setCompanyMember] = useState({});
+    const [companyMember, setCompanyDetail] = useState({});
     useEffect(() => {
+        console.log(companyMember);
     }, [companyMember]);
 
     useEffect(() => {
-        setCompanyMember({
+        setCompanyDetail({
             ...paramsData,
             userSn: paramsData?.userSn || '',
             userId: paramsData?.userId || '',
@@ -59,8 +110,68 @@ const CompanyModifyMember = ({data, onSave}) => {
             addr: paramsData?.addr || '',
             daddr: paramsData?.daddr || '',
             email: paramsData?.email || '',
+            mdfrSn: sessionUserSn,
         });
     }, [paramsData]);
+
+
+    const updateMember = () => {
+        if (!companyMember.userId) {
+            Swal.fire("회원ID를 입력해주세요.");
+            return false;
+        }
+
+        if (!companyMember.kornFlnm) {
+            Swal.fire("성명을 입력해주세요.");
+            return false;
+        }
+
+        if (!companyMember.mblTelno) {
+            Swal.fire("핸드폰번호를 입력해주세요.");
+            return false;
+        }
+
+        if (!companyMember.email) {
+            Swal.fire("이메일은 필수 값입니다.");
+            return false;
+        }
+
+        setCompanyDetail({...companyMember});
+
+
+        Swal.fire({
+            title: "저장하시겠습니까?",
+            showCloseButton: true,
+            showCancelButton: true,
+            confirmButtonText: "저장",
+            cancelButtonText: "취소"
+        }).then((result) => {
+            if(result.isConfirmed) {
+                const requestOptions = {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(companyMember)
+                };
+
+                EgovNet.requestFetch("/memberApi/setNormalMember", requestOptions, (resp) => {
+                    if (Number(resp.resultCode) === Number(CODE.RCV_SUCCESS)) {
+                        Swal.fire("수정되었습니다.").then((result) => {
+                        ComScript.closeModal("modifyModal");
+                        window.location.reload();
+                        });
+                    } else {
+                    }
+                });
+            } else {
+                //취소
+            }
+        });
+
+    };
+    
+    
 
 
     return (
@@ -98,6 +209,7 @@ const CompanyModifyMember = ({data, onSave}) => {
                                         <input
                                             type="password"
                                             defaultValue={companyMember?.userPw || ''}
+                                            readOnly
                                         />
                                         <button type="button" className="pwdBtn btn" onClick={(e) => {
                                             pwdReset();
@@ -140,7 +252,7 @@ const CompanyModifyMember = ({data, onSave}) => {
                                             type="text"
                                             defaultValue={companyMember?.email || ''}
                                             onChange={(e) =>
-                                                setCompanyMember({...companyMember, email: e.target.value})
+                                                setCompanyDetail({...companyMember, email: e.target.value})
                                             }
                                         />
                                     </div>
@@ -196,7 +308,7 @@ const CompanyModifyMember = ({data, onSave}) => {
                                 </li>
                             </ul>
                         </div>
-                        <button type="button" className="clickBtn black writeBtn">
+                        <button type="button" className="clickBtn black writeBtn" onClick={() => updateMember()}>
                             <span>수정</span></button>
                     </div>
                 </div>
