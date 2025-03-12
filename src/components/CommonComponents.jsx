5eeb4fd59989eefdafe2eb266e6b467984726b85
minjoo <ignore4557@gmail.com>
2025-03-12 13:25:02 +0900

    엑셀업로드 예시 롤백

--- src/components/CommonComponents.jsx
+++ src/components/CommonComponents.jsx
@@ -274,12 +274,6 @@ export const excelExport = async (exportFileName, sheetDatas) => {
             }
         });
 
-        //예시샘플
-        if (sheetData.example) {
-            worksheet.addRow(sheetData.example);
-        }
-        //예시샘플끝
-
         sheetData.row.forEach(item => {
             worksheet.addRow(Object.values(item));
         });
