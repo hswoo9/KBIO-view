1fb0ea18e64283922b2c51059d20203b90c97ea1
hswoo9 <gksthe@naver.com>
2025-04-14 10:59:38 +0900

    기관소개 이미지 사이즈 100으로 수정

--- src/pages/introduce/OperationalList.jsx
+++ src/pages/introduce/OperationalList.jsx
@@ -83,7 +83,7 @@ function OperationalList() {
                                     <figure>
                                         {item.tblComFile != null ? (
                                             <img
-                                                src={`http://${hostName}${item.tblComFile.atchFilePathNm}/${item.tblComFile.strgFileNm}.${item.tblComFile.atchFileExtnNm}`} width="44" height="44"
+                                                src={`http://${hostName}${item.tblComFile.atchFilePathNm}/${item.tblComFile.strgFileNm}.${item.tblComFile.atchFileExtnNm}`} width="100" height="44"
                                                 alt={`${item.tblMvnEnt.mvnEntNm}_로고`}
                                             />
                                         ) : (
